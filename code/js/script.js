2f5344d8413d02fdeda5ebbb424a268b1d1bc0ae
SafaeQ <safaedani009@gmail.com>
2021-02-17 16:43:38 +0100

    last add

--- code/js/script.js
+++ code/js/script.js
@@ -1,1 +1,26 @@
+ 
+const btnMenu = document.querySelector('.btn-rond');
+const nav = document.querySelector('.menu-bar');
+const close = document.querySelector('.close-menu-btn')
+
+
+
+
+
+btnMenu.addEventListener('click', ()=>{
+
+
+    nav.classList.add('show-nav');
+
+});
+
+close.addEventListener('click', ()=>{
+
+
+    nav.classList.remove('show-nav');
+
+});
+
+
+
 
